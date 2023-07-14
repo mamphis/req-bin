3e9eac27dc777b2928ca13836cbbce55cc602782
mamphis <mattis.wacker@pcsmw.de>
2023-07-14 22:37:07 +0200

    refactor: removed external dependencies and serve css from own server

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,7 @@ app.set('views', join(__dirname, '../client'));
 app.enable('trust proxy');
 app.use('/static/js', express.static(join(__dirname, '../client/js')));
 app.use('/static/css', express.static(join(__dirname, '../client/css')));
+app.use('/static/css', express.static(join(__dirname, '../node_modules/bulma/css')));
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
     const id = v4()
